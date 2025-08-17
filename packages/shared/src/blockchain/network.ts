045ba5b578cc2c0cea387cc344661c983494ff74
signature18632 <intmax18@gmail.com>
2025-08-17 20:16:04 +0700

    refactor config

--- packages/shared/src/blockchain/network.ts
+++ packages/shared/src/blockchain/network.ts
@@ -1,38 +1,24 @@
 import { type Chain, createPublicClient, http, type PublicClient } from "viem";
-import { mainnet, scroll, scrollSepolia, sepolia } from "viem/chains";
+import { mainnet, sepolia } from "viem/chains";
 import { config } from "../config";
 
 export const networkConfig = {
-  ethereum: {
-    mainnet: {
-      chain: mainnet,
-      rpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${config.ALCHEMY_API_KEY}`,
-    },
-    sepolia: {
-      chain: sepolia,
-      rpcUrl: `https://eth-sepolia.g.alchemy.com/v2/${config.ALCHEMY_API_KEY}`,
-    },
+  mainnet: {
+    chain: mainnet,
   },
-  scroll: {
-    mainnet: {
-      chain: scroll,
-      rpcUrl: `https://scroll-mainnet.g.alchemy.com/v2/${config.ALCHEMY_API_KEY}`,
-    },
-    sepolia: {
-      chain: scrollSepolia,
-      rpcUrl: `https://scroll-sepolia.g.alchemy.com/v2/${config.ALCHEMY_API_KEY}`,
-    },
+  testnet: {
+    chain: sepolia,
   },
 };
 
-export const createNetworkClient = (network: "ethereum" | "scroll") => {
-  const { chain, rpcUrl } = networkConfig[network][config.NETWORK_ENVIRONMENT];
+export const createNetworkClient = () => {
+  const { chain } = networkConfig[config.NETWORK_ENVIRONMENT];
 
   return createPublicClient({
     batch: {
       multicall: true,
     },
     chain: chain as Chain,
-    transport: http(rpcUrl),
+    transport: http(config.L1_RPC_URL),
   }) as PublicClient;
 };
